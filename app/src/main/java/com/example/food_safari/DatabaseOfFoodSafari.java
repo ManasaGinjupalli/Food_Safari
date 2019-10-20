32cff94c3204dde0c20194eff7b00c8963d1e5b0
Raghavendra <raghavendarm10@gmail.com>
2019-10-20 15:42:26 -0500

    Created table for resturant.

--- app/src/main/java/com/example/food_safari/DatabaseOfFoodSafari.java
+++ app/src/main/java/com/example/food_safari/DatabaseOfFoodSafari.java
@@ -7,20 +7,31 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseOfFoodSafari extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "Food Safari DataBase";
+    public static final String DATABASE_NAME = "FoodSafariDataBase.db";
+    public static final String TABLE_NAME = "Resturant";
+    public static final String Col_1 = "Rest_ID";
+    public static final String Col_2 = "Rest_Name";
+    public static final String Col_3 = "Rest_Address";
+    public static final String Col_4 = "Rest_Availability";
 
 
-    public DatabaseOfFoodSafari(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    public DatabaseOfFoodSafari(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create Table "+TABLE_NAME + " (Rest_ID INTEGER PRIMARY KEY AUTOINCREMENT, Rest_Name TEXT, Rest_Adress TEXT, Rest_Availability TEXT )" );
+
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(" Drop Table "+TABLE_NAME);
+        onCreate(db);
 
     }
 }

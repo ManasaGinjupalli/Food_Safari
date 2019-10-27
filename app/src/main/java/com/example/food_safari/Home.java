276d4cf4bb714e78487c0fef921665beebac1371
saikirandd <33051183+saikirandd@users.noreply.github.com>
2019-10-27 18:20:09 -0500

    Updated login.java and home.java by sai kiran

--- app/src/main/java/com/example/food_safari/Home.java
+++ app/src/main/java/com/example/food_safari/Home.java
@@ -2,13 +2,38 @@ package com.example.food_safari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+    Button medBTN,mongBTN,indBTN,MexiBTN,splOfBTN,splDish,trackBTN,bckBtn;
+    Intent b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        medBTN=findViewById(R.id.medbtn);
+        medBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b1=new Intent(Home.this,Restaurant.class);
+                startActivity(b1);
+
+            }
+        });
+        bckBtn=findViewById(R.id.backBTN);
+        bckBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b2=new Intent(Home.this,LoginActivity.class);
+                startActivity(b2);
+            }
+        });
+
+
     }
+
 }

9bce41222ec404626756c70d363e6cad6c681cd5
saikirandd <33051183+saikirandd@users.noreply.github.com>
2019-10-27 14:29:09 -0500

    widgets updated by sai kiran

--- app/src/main/java/com/example/food_safari/MainActivity.java
+++ app/src/main/java/com/example/food_safari/MainActivity.java
@@ -2,15 +2,35 @@ package com.example.food_safari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseOfFoodSafari myFoodSafari;
+    Button b;
+    Intent b1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myFoodSafari = new DatabaseOfFoodSafari(this);
+        b= (Button)findViewById(R.id.MainBTN);
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+               b1 = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(b1);
+
+            }
+        });
+
     }
+
+
 }

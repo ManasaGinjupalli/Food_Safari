cd9104cc2f64accebcb29847744ecad5ef5da8cd
Nishanthreddy1997 <46684557+Nishanthreddy1997@users.noreply.github.com>
2019-10-27 21:19:02 -0500

    udpated layouts and java code by Nishanth Reddy Devi Reddy

--- app/src/main/java/com/example/food_safari/Menu.java
+++ app/src/main/java/com/example/food_safari/Menu.java
@@ -2,13 +2,36 @@ package com.example.food_safari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+    Button pastaBTN,peproniBTN,back;
+    Intent b1,b2,b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        pastaBTN=findViewById(R.id.PastaBTN);
+        pastaBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b1=new Intent(Menu.this,Cart.class);
+                startActivity(b1);
+            }
+        });
+
+
+        back=findViewById(R.id.backAGBTN);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b3=new Intent(Menu.this,Restaurant.class);
+                startActivity(b3);
+            }
+        });
     }
 }

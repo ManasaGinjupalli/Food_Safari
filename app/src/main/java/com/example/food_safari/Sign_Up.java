4ecf6d11bd9f463cf63e181dbc11f86834aaa938
pradeepkumar <pradeepkumartheegala@gmail.com>
2019-11-01 13:32:58 -0500

    Updated the signup activity and name of the app

--- app/src/main/java/com/example/food_safari/Sign_Up.java
+++ app/src/main/java/com/example/food_safari/Sign_Up.java
@@ -1,6 +1,9 @@
 package com.example.food_safari;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class Sign_Up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button createBTN = findViewById(R.id.RegisterBTN);
+        createBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(Sign_Up.this, LoginActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
